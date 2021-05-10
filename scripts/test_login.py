7f8906983be3869eb14451b254122988e4059116
Pan <3038459088@qq.com>
2021-05-10 22:18:03 +0800

    go

---
+++ scripts/test_login.py
@@ -0,0 +1,11 @@
+class Testlogin:
+    def test_lo1(self):
+        assert 1
+
+    def test_lo2(self):
+        assert 0
+
+    def test_lo3(self):
+        assert 0
+
+# https://github.com/Panf61/jenkins-pro.git
